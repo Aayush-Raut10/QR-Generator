78523f0ecdad2372aaab447007a7e438fbfea8d7
Aayush-Raut10 <aayushraut209@gmail.com>
2026-01-07 23:15:55 +0545

    implementing all backend logic

--- Generator/views.py
+++ Generator/views.py
@@ -1,11 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import qrcode
+import datetime
+import os
+from django.conf import settings
 
 # Create your views here.
 
 def index(request):
-    return render(request, "Generator/index.html")
+    if request.method == "POST":
+        
+
+        name = request.POST.get("name")
+        url = request.POST.get("url")
+
+        if not name or not url:
+
+            context = {
+                "error":0
+            }
+
+            return render(request, "Generator/index.html", context=context)
+
+        else:
+
+            newQr = qrcode.make(url)
+
+            time = str(datetime.datetime.now())
+
+            filename = name.replace(" ", "").lower() + time.replace(" ", "").replace("-", "_").replace(":","_")  + ".png"
+
+            path = os.path.join(settings.MEDIA_ROOT, filename)
+
+            newQr.save(path)
+
+            # create QR code URL
+            qrUrl = os.path.join(settings.MEDIA_URL, filename)
+
+            return render(request, "Generator/result.html", context= {
+                "name":name,
+                "qrurl":qrUrl,
+                "filename":filename
+            }
+)
+           
+
+    else:
+        
+        return render(request, "Generator/index.html")
 
 
-def generate(request):
-    return render(request, "Generator/")
+
